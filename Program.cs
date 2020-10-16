e6625902555174a491e139670395deebc62b45f0
Suman Kumar Shee <suman.shee@globant.com>
2020-10-17 01:42:44 +0530

    test

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Azure.Identity;
 
 namespace DotNetCoreSqlDb
 {
@@ -20,6 +21,13 @@ namespace DotNetCoreSqlDb
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+.ConfigureAppConfiguration((context, config) =>
+{
+var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+config.AddAzureKeyVault(
+keyVaultEndpoint,
+new DefaultAzureCredential());
+})
                 .ConfigureLogging(logging =>
                 {
                     logging.AddAzureWebAppDiagnostics();
